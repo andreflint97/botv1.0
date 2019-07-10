21722416dfacfb957fe1cba6dee3f4f14d9e6edc
andre flint <andreflint97@icloud.com>
2019-07-10 12:21:56 -0400

    changes

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -2,11 +2,11 @@ var path = require("path");
 var exports = module.exports = {}
 
 exports.signin = function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 }
 
 exports.home = function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/home.html"));
+  res.sendFile(path.join(__dirname, "public/home.html"));
 }
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -16,21 +16,20 @@ module.exports = function (app, passport) {
     app.get('/', authController.signin);
     app.get('/home', authController.signin);
     app.get('/login', authController.signin);
-    // meal planner html route
-    app.get('/home',isLoggedIn, authController.mealplanner);
+ 
     //////////////////// HOME /////////////////////////////////////////////////////////////////////////////
     app.get('/home', isLoggedIn, authController.home);
 
     //////////////////// POST NEW USER ////////////////////////////////////////////////////////////////////
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect: '/home',
-        failureRedirect: '/signin'
+        failureRedirect: '/index'
     }));
     //////////////////// LOGOUT ///////////////////////////////////////////////////////////////////////////
     app.get('/logout', authController.logout);
     //////////////////// SIGN IN POST /////////////////////////////////////////////////////////////////////
     app.post('/signin', passport.authenticate('local-signin', {
         successRedirect: '/home',
-        failureRedirect: '/signin'
+        failureRedirect: '/index'
     }));
 }
